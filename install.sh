25f2314be41e0b049a41943246bff7da1a4d5082
Daniel Høier Øhrgaard <daniel@yoke.dk>
2017-12-18 17:41:40 +0100

    Add devicetree compilation and installation to install script

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ if [ "$EUID" -ne 0 ]; then
   exit
 fi
 
+DT_FILE=klogskabet-audio-buttons
+
 # Copy logrotate script into position
 sudo cp ./config/klogskabet-audio-player.logrotate /etc/logrotate.d/klogskabet-audio-player.logrotate
 sudo chmod 744 /etc/logrotate.d/klogskabet-audio-player.logrotate
@@ -18,3 +20,14 @@ sudo chmod 644 /lib/systemd/system/klogskabet-audio-player.service
 sudo systemctl daemon-reload
 sudo systemctl enable klogskabet-audio-player.service
 sudo systemctl start klogskabet-audio-player.service
+
+# Add device tree if it doesn't already exist
+sudo grep "dtoverlay=$DT_FILE" /boot/config.txt
+if [[ $? -eq 1 ]]; then
+  # Compile device tree
+  dtc -W no-unit_address_vs_reg -@ -I dts -O dtb -o ./devicetree/$DT_FILE.dtbo ./devicetree/$DT_FILE.dts
+  sudo cp -f ./devicetree/$DT_FILE.dtbo /boot/overlays/$DT_FILE.dtbo
+  sudo printf "\n\ndtoverlay=$DT_FILE\n" >> /boot/config.txt
+else
+  echo "Device tree already installed, skipping"
+fi
