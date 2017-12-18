b84ac2f01b6f9706ace3c10e7ac2ca27d8b8a4e2
Daniel Høier Øhrgaard <daniel@yoke.dk>
2017-12-18 11:40:39 +0100

    Hook up buttons as player controls

--- index.js
+++ index.js
@@ -7,14 +7,17 @@ const nextButton = new Button(22);
 
 prevButton.on('press', _ => {
   console.log('Prev pressed');
+  playPrevious();
 });
 
 playPauseButton.on('press', _ => {
   console.log('Play/pause pressed');
+  player.playPause();
 });
 
 nextButton.on('press', _ => {
   console.log('Next pressed');
+  playNext();
 });
 
 // a simple helper function
@@ -65,6 +68,23 @@ function playNext() {
 
   player.load(track);
   list.push(track);
+}
+
+// grab the last track in the array, load and play it, and put it back in
+// the list at the head of the list
+function playPrevious() {
+  if (!list.length) {
+    console.warn("No tracks!");
+    return;
+  }
+
+  var track = list.pop();
+  player.once('playing', function () {
+    console.log(`Playing ${track}`);
+  });
+
+  player.load(track);
+  list.unshift(track);
 }
 
 // when playback stops, play the next track
